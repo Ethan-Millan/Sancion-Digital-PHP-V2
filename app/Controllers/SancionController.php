fd3412e8885c0e771dbff00f760e75683e35ce95
ethan <ethan@2806-104e-002c-66d2-6e1f-f7ff-fe1a-a9c5.ipv6.infinitum.net.mx>
2026-04-28 16:29:19 -0600

    feat: SancionController con validaciones y store completo

--- app/Controllers/SancionController.php
+++ app/Controllers/SancionController.php
@@ -22,25 +22,35 @@ class SancionController{
     public function store_validation(){
         try{
             $errores = [];
-            $sanciones = $_POST['sancion'];
 
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $sanciones = $_POST['sancion'] ?? [];
                 if(empty($sanciones)){
-                    throw new ValidationException('No se pueden enviar formularios vacios.');
+                    throw new ValidationException('No se pueden enviar formularios vacios.');//Valida que el array no venga vacio
                 }
 
                 foreach($sanciones as $sancion => $datos){
-                    if(empty($datos['alumno_matricula']) || empty($datos['vigilante_matricula'])){
-                        $errores[] = 'La matriculas no pueden estar vacias ';
+                    $datos['id_alumno'] = trim($datos['id_alumno']);
+                    $datos['alumno_matricula'] = trim($datos['alumno_matricula']); 
+                    $datos['id_vigilante'] = trim($datos['id_vigilante']);
+                    $datos['vigilante_matricula'] = trim($datos['vigilante_matricula']); 
+                    $datos['codigo_falta_id'] = trim($datos['codigo_falta_id']);
+                    $datos['observaciones'] = trim($datos['observaciones']);
+
+                    if(empty($datos['alumno_matricula']) || empty($datos['vigilante_matricula'])){//Verifica que las matricuals no esten vacias 
+                        $errores[] = 'La matriculas no pueden estar vacias ';//si estan vacias gaurda el error 
+                        continue;//Si este error existe no vale la pena continuar con el resto asi que se salta al proximo cilo del foreach
                     }
 
                     $alumno = $this->usuariosModel->BuscarUsuario($datos['alumno_matricula'],$datos['id_alumno']);
+                    //Busca al usaurio en la base de datos validando que su matricula pertenezca a su id 
 
                     if(!$alumno){
                         $errores[] = 'El alumno no existe';
                     }
                     
                     $vigilante = $this->usuariosModel->BuscarUsuario($datos['vigilante_matricula'], $datos['id_vigilante']);
+                    //Busca al vigilante en la base de datos validando que su matriual pertenezca a su id 
 
                     if(!$vigilante){
                         $errores[] = 'El vigilante no existe';
@@ -57,10 +67,10 @@ class SancionController{
                 }
 
                 if(!empty($errores)){
-                    throw new ValidationException('Se cuanta con lo siguientes errores: ' . $errores);
+                    throw new ValidationException('Se cuanta con lo siguientes errores: ' . implode(', ',  $errores));
                 }
 
-                Store($sanciones);
+                $this->store($sanciones);
             }
         }catch(ValidationException $e){
             $_SESSION['error'] = $e->getMessage();
@@ -72,7 +82,17 @@ class SancionController{
     }
 
     private function store($sanciones){
+        foreach($sanciones  as $sancion){
+            if(!$this->sancionModel->store($sancion)){
+                $_SESSION['error'] = 'Fallo al crear la sancion';
+                header('Location: ' . URL_PROJECT . 'index.php?url=sancion/index');
+                exit;
+            }
+        }
 
+        $_SESSION['success'] = 'Sancion creada con exito';
+        header('Location: ' . URL_PROJECT . 'index.php?url=sancion/index');
+        exit;
     }
 
     public function update(){
