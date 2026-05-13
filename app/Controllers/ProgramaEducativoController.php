674955cac5a2f879c19e4ef2671a6224954b31f0
Ethan-Millán <millan.diaz.ethan@gmail.com>
2026-05-12 23:11:58 -0600

    feat: Creacion de estructura de nuevo controlador Programas Educativos
    Creacion de funciones pricipales para lo que es Programas Educativos
    (CRUD).

---
+++ app/Controllers/ProgramaEducativoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\Controllers;
+
+class ProgramaEducativo
+{
+    private $db;
+    private $prog_educ;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function index()
+    {
+        return false;
+    }
+    public function store()
+    {
+        return false;
+    }
+    public function update()
+    {
+        return false;
+    }
+    public function delete()
+    {
+        return false;
+    }
+}
